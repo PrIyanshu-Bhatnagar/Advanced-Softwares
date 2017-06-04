3e46a501a9062b54629e1d28aa422bc6bf393bb6
PrIyanshu-Bhatnagar <priyanshubhatnagar1851996@gmail.com>
2017-06-04 19:48:57 +0530

    Update

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
+var aboutUsText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis pellentesque lacus eleifend lacinia Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis pellentesque lacus eleifend laciniaLorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis pellentesque lacus eleifend laciniaLorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis pellentesque lacus eleifend laciniaLorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis pellentesque lacus eleifend laciniaLorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis pellentesque lacus eleifend laciniaLorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis pellentesque lacus eleifend laciniaLorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis pellentesque lacus eleifend laciniaLorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis pellentesque lacus eleifend laciniaLorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis pellentesque lacus eleifend lacinia...";
+
 $(document).ready(function()
 {
+    var mainWidth = $(window).width() - 20;
+    $(".mdl-layout__content").css('width', mainWidth);
 
-    var width = $(window).innerWidth() - 20;
-    $(".demo-card-wide").css('width', width);
-
-    var mainDiv = $(".mdl-grid");
+    var mainDiv = $("#main_grid");
+    var aboutDiv = $("#about_us");
     //Desktop Screen size
     if (/*$(window).height() >= 768 && */$(window).width() >= 1024)
     {
         $('header').removeClass("mdl-layout--small-screen-only").addClass("mdl-layout--large-screen-only");
+
+        aboutDiv.append("<div class=\"mdl-cell mdl-cell--4-col\"></div>");
+        aboutDiv.append("<div class=\"mdl-cell mdl-cell--4-col\"><div class=\"demo-card-wide mdl-card mdl-shadow--2dp\"><div class=\"mdl-card__supporting-text\"><h1>About Us</h1><p>"+aboutUsText+ "</p></div></div></div>")
+        aboutDiv.append("<div class=\"mdl-cell mdl-cell--4-col\"></div>");
 
         mainDiv.append("<div class=\"mdl-cell mdl-cell--6-col\" style=\"background-color: red;\">6</div>");
         mainDiv.append("<div class=\"mdl-cell mdl-cell--6-col\" style=\"background-color: red;\">6</div>");
@@ -18,6 +24,8 @@ $(document).ready(function()
     {
         $('header').removeClass("mdl-layout--small-screen-only").addClass("mdl-layout--large-screen-only");
 
+        aboutDiv.append("<div class=\"mdl-cell\"><div class=\"demo-card-wide mdl-card mdl-shadow--2dp\"><div class=\"mdl-card__supporting-text\"><h1>About Us</h1><p>"+aboutUsText+ "</p></div></div></div>")
+
         mainDiv.append("<div class=\"mdl-cell mdl-cell--4-col\" style=\"background-color: red;\">4</div>");
         mainDiv.append("<div class=\"mdl-cell mdl-cell--4-col\" style=\"background-color: red;\">4</div>");
     }
@@ -25,6 +33,8 @@ $(document).ready(function()
     else
     {
         $('header').removeClass("mdl-layout--large-screen-only").addClass("mdl-layout--small-screen-only");
+
+        aboutDiv.append("<div class=\"mdl-cell\"><div class=\"demo-card-wide mdl-card mdl-shadow--2dp\"><div class=\"mdl-card__supporting-text\"><h1>About Us</h1><p>"+aboutUsText+ "</p></div></div></div>")
 
         mainDiv.append("<div class=\"mdl-cell mdl-cell--2-col\" style=\"background-color: red;\">2</div>");
         mainDiv.append("<div class=\"mdl-cell mdl-cell--2-col\" style=\"background-color: red;\">2</div>");
@@ -33,15 +43,22 @@ $(document).ready(function()
 });
 
 $( window ).resize(function() {
-    var width = $(window).innerWidth() - 20;
-    $(".demo-card-wide").css('width', width);
+    var aboutDiv = $("#about_us");
+    aboutDiv.empty();
 
-    var mainDiv = $(".mdl-grid");
+    var mainWidth = $(window).width() - 20;
+    $(".mdl-layout__content").css('width', mainWidth);
+
+    var mainDiv = $("#main_grid");
     mainDiv.empty();
     //Desktop Screen size
     if (/*$(window).height() >= 768 && */$(window).width() >= 1024)
     {
         $('header').removeClass("mdl-layout--small-screen-only").addClass("mdl-layout--large-screen-only");
+
+        aboutDiv.append("<div class=\"mdl-cell mdl-cell--4-col\"></div>");
+        aboutDiv.append("<div class=\"mdl-cell mdl-cell--4-col\"><div class=\"demo-card-wide mdl-card mdl-shadow--2dp\"><div class=\"mdl-card__supporting-text\"><h1>About Us</h1><p>"+aboutUsText+ "</p></div></div></div>")
+        aboutDiv.append("<div class=\"mdl-cell mdl-cell--4-col\"></div>");
 
         mainDiv.append("<div class=\"mdl-cell mdl-cell--6-col\" style=\"background-color: red;\">6</div>");
         mainDiv.append("<div class=\"mdl-cell mdl-cell--6-col\" style=\"background-color: red;\">6</div>");
@@ -51,6 +68,8 @@ $( window ).resize(function() {
     {
         $('header').removeClass("mdl-layout--small-screen-only").addClass("mdl-layout--large-screen-only");
 
+        aboutDiv.append("<div class=\"mdl-cell\"><div class=\"demo-card-wide mdl-card mdl-shadow--2dp\"><div class=\"mdl-card__supporting-text\"><h1>About Us</h1><p>"+aboutUsText+ "</p></div></div></div>")
+
         mainDiv.append("<div class=\"mdl-cell mdl-cell--4-col\" style=\"background-color: red;\">4</div>");
         mainDiv.append("<div class=\"mdl-cell mdl-cell--4-col\" style=\"background-color: red;\">4</div>");
     }
@@ -58,6 +77,8 @@ $( window ).resize(function() {
     else
     {
         $('header').removeClass("mdl-layout--large-screen-only").addClass("mdl-layout--small-screen-only");
+
+        aboutDiv.append("<div class=\"mdl-cell\"><div class=\"demo-card-wide mdl-card mdl-shadow--2dp\"><div class=\"mdl-card__supporting-text\"><h1>About Us</h1><p>"+aboutUsText+ "</p></div></div></div>")
 
         mainDiv.append("<div class=\"mdl-cell mdl-cell--2-col\" style=\"background-color: red;\">2</div>");
         mainDiv.append("<div class=\"mdl-cell mdl-cell--2-col\" style=\"background-color: red;\">2</div>");

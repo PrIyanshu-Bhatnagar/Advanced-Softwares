a93ed6f6620af33dd1c1c0b9fb4860d2dfb6c198
PrIyanshu-Bhatnagar <priyanshubhatnagar1851996@gmail.com>
2017-06-04 16:14:07 +0530

    Update

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@ $(document).ready(function()
 {
     var mainDiv = $(".mdl-grid");
     //Desktop Screen size
-    if (window.screen.availHeight >= 768 && window.screen.availWidth >= 1024)
+    if (/*$(window).height() >= 768 && */$(window).width() >= 1024)
     {
         mainDiv.append("<div class=\"mdl-cell mdl-cell--6-col\" style=\"background-color: red;\">6</div>");
         mainDiv.append("<div class=\"mdl-cell mdl-cell--6-col\" style=\"background-color: red;\">6</div>");
     }
     //Desktop Tablet size
-    else if (window.screen.availHeight >= 660 && window.screen.availWidth >= 1010)
+    else if (/*$(window).height() >= 660 && */$(window).width() >= 1010)
     {
         mainDiv.append("<div class=\"mdl-cell mdl-cell--4-col\" style=\"background-color: red;\">4</div>");
         mainDiv.append("<div class=\"mdl-cell mdl-cell--4-col\" style=\"background-color: red;\">4</div>");
@@ -20,3 +20,28 @@ $(document).ready(function()
         mainDiv.append("<div class=\"mdl-cell mdl-cell--2-col\" style=\"background-color: red;\">2</div>");
     }
 });
+
+$( window ).resize(function() {
+    var mainDiv = $(".mdl-grid");
+    mainDiv.empty();
+    //Desktop Screen size
+    if (/*$(window).height() >= 768 && */$(window).width() >= 1024)
+    {
+        mainDiv.append("<div class=\"mdl-cell mdl-cell--6-col\" style=\"background-color: red;\">6</div>");
+        mainDiv.append("<div class=\"mdl-cell mdl-cell--6-col\" style=\"background-color: red;\">6</div>");
+    }
+    //Desktop Tablet size
+    else if (/*$(window).height() >= 660 && */$(window).width() >= 1010)
+    {
+        mainDiv.append("<div class=\"mdl-cell mdl-cell--4-col\" style=\"background-color: red;\">4</div>");
+        mainDiv.append("<div class=\"mdl-cell mdl-cell--4-col\" style=\"background-color: red;\">4</div>");
+    }
+    //Desktop Mobile size
+    else
+    {
+        mainDiv.append("<div class=\"mdl-cell mdl-cell--2-col\" style=\"background-color: red;\">2</div>");
+        mainDiv.append("<div class=\"mdl-cell mdl-cell--2-col\" style=\"background-color: red;\">2</div>");
+    }
+});
+
+

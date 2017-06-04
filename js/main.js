16b1f09d887e59a0e60cc5b3e579ac980068e8d6
PrIyanshu-Bhatnagar <priyanshubhatnagar1851996@gmail.com>
2017-06-04 21:21:16 +0530

    Screen Size adjustment

--- js/main.js
+++ js/main.js
@@ -7,34 +7,31 @@ $(document).ready(function()
 
     var mainDiv = $("#main_grid");
     var aboutDiv = $("#about_us");
+
+        aboutDiv.append("<div class=\"mdl-cell mdl-cell--3-col\"></div>");
+        aboutDiv.append("<div class=\"mdl-cell mdl-cell--6-col\"><div class=\"demo-card-wide mdl-card mdl-shadow--2dp\"><div class=\"mdl-card__supporting-text\"><h1>About Us</h1><p>"+aboutUsText+ "</p></div></div></div>")
+        aboutDiv.append("<div class=\"mdl-cell mdl-cell--3-col\"></div>");
+
     //Desktop Screen size
     if (/*$(window).height() >= 768 && */$(window).width() >= 1024)
     {
         $('header').removeClass("mdl-layout--small-screen-only").addClass("mdl-layout--large-screen-only");
 
-        aboutDiv.append("<div class=\"mdl-cell mdl-cell--4-col\"></div>");
-        aboutDiv.append("<div class=\"mdl-cell mdl-cell--4-col\"><div class=\"demo-card-wide mdl-card mdl-shadow--2dp\"><div class=\"mdl-card__supporting-text\"><h1>About Us</h1><p>"+aboutUsText+ "</p></div></div></div>")
-        aboutDiv.append("<div class=\"mdl-cell mdl-cell--4-col\"></div>");
-
         mainDiv.append("<div class=\"mdl-cell mdl-cell--6-col\" style=\"background-color: red;\">6</div>");
         mainDiv.append("<div class=\"mdl-cell mdl-cell--6-col\" style=\"background-color: red;\">6</div>");
     }
-    //Desktop Tablet size
-    else if (/*$(window).height() >= 660 && */$(window).width() >= 1010)
+    //Tablet size
+    else if (/*$(window).height() >= 660 && */$(window).width() >= 550)
     {
         $('header').removeClass("mdl-layout--small-screen-only").addClass("mdl-layout--large-screen-only");
 
-        aboutDiv.append("<div class=\"mdl-cell\"><div class=\"demo-card-wide mdl-card mdl-shadow--2dp\"><div class=\"mdl-card__supporting-text\"><h1>About Us</h1><p>"+aboutUsText+ "</p></div></div></div>")
-
         mainDiv.append("<div class=\"mdl-cell mdl-cell--4-col\" style=\"background-color: red;\">4</div>");
         mainDiv.append("<div class=\"mdl-cell mdl-cell--4-col\" style=\"background-color: red;\">4</div>");
     }
-    //Desktop Mobile size
+    //Mobile size
     else
     {
         $('header').removeClass("mdl-layout--large-screen-only").addClass("mdl-layout--small-screen-only");
-
-        aboutDiv.append("<div class=\"mdl-cell\"><div class=\"demo-card-wide mdl-card mdl-shadow--2dp\"><div class=\"mdl-card__supporting-text\"><h1>About Us</h1><p>"+aboutUsText+ "</p></div></div></div>")
 
         mainDiv.append("<div class=\"mdl-cell mdl-cell--2-col\" style=\"background-color: red;\">2</div>");
         mainDiv.append("<div class=\"mdl-cell mdl-cell--2-col\" style=\"background-color: red;\">2</div>");
@@ -46,6 +43,10 @@ $( window ).resize(function() {
     var aboutDiv = $("#about_us");
     aboutDiv.empty();
 
+        aboutDiv.append("<div class=\"mdl-cell mdl-cell--3-col\"></div>");
+        aboutDiv.append("<div class=\"mdl-cell mdl-cell--6-col\"><div class=\"demo-card-wide mdl-card mdl-shadow--2dp\"><div class=\"mdl-card__supporting-text\"><h1>About Us</h1><p>"+aboutUsText+ "</p></div></div></div>")
+        aboutDiv.append("<div class=\"mdl-cell mdl-cell--3-col\"></div>");
+
     var mainWidth = $(window).width() - 20;
     $(".mdl-layout__content").css('width', mainWidth);
 
@@ -56,29 +57,25 @@ $( window ).resize(function() {
     {
         $('header').removeClass("mdl-layout--small-screen-only").addClass("mdl-layout--large-screen-only");
 
-        aboutDiv.append("<div class=\"mdl-cell mdl-cell--4-col\"></div>");
-        aboutDiv.append("<div class=\"mdl-cell mdl-cell--4-col\"><div class=\"demo-card-wide mdl-card mdl-shadow--2dp\"><div class=\"mdl-card__supporting-text\"><h1>About Us</h1><p>"+aboutUsText+ "</p></div></div></div>")
-        aboutDiv.append("<div class=\"mdl-cell mdl-cell--4-col\"></div>");
-
         mainDiv.append("<div class=\"mdl-cell mdl-cell--6-col\" style=\"background-color: red;\">6</div>");
         mainDiv.append("<div class=\"mdl-cell mdl-cell--6-col\" style=\"background-color: red;\">6</div>");
     }
-    //Desktop Tablet size
-    else if (/*$(window).height() >= 660 && */$(window).width() >= 1010)
+    //Tablet size
+    else if (/*$(window).height() >= 660 && */$(window).width() >= 550)
     {
         $('header').removeClass("mdl-layout--small-screen-only").addClass("mdl-layout--large-screen-only");
 
-        aboutDiv.append("<div class=\"mdl-cell\"><div class=\"demo-card-wide mdl-card mdl-shadow--2dp\"><div class=\"mdl-card__supporting-text\"><h1>About Us</h1><p>"+aboutUsText+ "</p></div></div></div>")
+//        aboutDiv.append("<div class=\"mdl-cell\" style=\"width: 100%\"><div class=\"demo-card-wide mdl-card mdl-shadow--2dp\"><div class=\"mdl-card__supporting-text\"><h1>About Us</h1><p>"+aboutUsText+ "</p></div></div></div>")
 
         mainDiv.append("<div class=\"mdl-cell mdl-cell--4-col\" style=\"background-color: red;\">4</div>");
         mainDiv.append("<div class=\"mdl-cell mdl-cell--4-col\" style=\"background-color: red;\">4</div>");
     }
-    //Desktop Mobile size
+    //Mobile size
     else
     {
         $('header').removeClass("mdl-layout--large-screen-only").addClass("mdl-layout--small-screen-only");
 
-        aboutDiv.append("<div class=\"mdl-cell\"><div class=\"demo-card-wide mdl-card mdl-shadow--2dp\"><div class=\"mdl-card__supporting-text\"><h1>About Us</h1><p>"+aboutUsText+ "</p></div></div></div>")
+//        aboutDiv.append("<div class=\"mdl-cell\" style=\"width: 100%\"><div class=\"demo-card-wide mdl-card mdl-shadow--2dp\"><div class=\"mdl-card__supporting-text\"><h1>About Us</h1><p>"+aboutUsText+ "</p></div></div></div>")
 
         mainDiv.append("<div class=\"mdl-cell mdl-cell--2-col\" style=\"background-color: red;\">2</div>");
         mainDiv.append("<div class=\"mdl-cell mdl-cell--2-col\" style=\"background-color: red;\">2</div>");

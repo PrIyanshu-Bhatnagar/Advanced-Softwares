e68e049c196f43fda7052c4b262b270894623aca
PrIyanshu-Bhatnagar <priyanshubhatnagar1851996@gmail.com>
2017-06-05 11:21:45 +0530

    Products Updates

--- js/main.js
+++ js/main.js
@@ -31,6 +31,7 @@ function mainDivison()
     mainDiv.append("<div class=\"mdl-cell mdl-cell--12-col\"><div class=\"demo-card-wide mdl-card mdl-shadow--2dp\"><div class=\"mdl-card__supporting-text\" id=\"product_grid\" style=\"width:100%; padding:0px\"></div></div></div>");
 
     mainDiv = $("#product_grid");
+    mainDiv.append("<h1 style=\"margin: 24px 0px 0px 0px; padding: 0px 0px 0px 20px;\">Products</h1>");
     mainDiv.append("<div class=\"mdl-grid\" id=\"products\"></div>");
     mainDiv = $('#products');
     //Desktop Screen size
@@ -73,7 +74,6 @@ function cardWidthAdjustment() {
 function develop_Product() {
     var product_image = $("#product_image");
     var product_details = $("#product_details");
-
     createCard(product_image,"product_image");
     createCard(product_details, "product_details");
 }

95c229239ce7da30b564889dac473d15eca13a66
PrIyanshu-Bhatnagar <priyanshubhatnagar1851996@gmail.com>
2017-06-05 12:15:46 +0530

    Opacity Changed

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ function aboutDivison()
     var aboutDiv = $("#about_us");
     aboutDiv.empty();
     aboutDiv.append("<div class=\"mdl-cell mdl-cell--3-col\"></div>");
-    aboutDiv.append("<div class=\"mdl-cell mdl-cell--6-col\"><div class=\"demo-card-wide mdl-card mdl-shadow--2dp\" style=\"background: transparent\"><div class=\"mdl-card__supporting-text\"><h1>About Us</h1><p>"+aboutUsText+ "</p></div></div></div>")
+    aboutDiv.append("<div class=\"mdl-cell mdl-cell--6-col\"><div class=\"demo-card-wide mdl-card mdl-shadow--2dp\" style=\"background: rgba(0,0,0,0.12)\"><div class=\"mdl-card__supporting-text\"><h1>About Us</h1><p>"+aboutUsText+ "</p></div></div></div>")
     aboutDiv.append("<div class=\"mdl-cell mdl-cell--3-col\"></div>");
 
 }
@@ -28,7 +28,7 @@ function mainDivison()
 {
     var mainDiv = $("#main_grid");
     mainDiv.empty();
-    mainDiv.append("<div class=\"mdl-cell mdl-cell--12-col\"><div class=\"demo-card-wide mdl-card mdl-shadow--2dp\" style=\"background: transparent\"><div class=\"mdl-card__supporting-text\" id=\"product_grid\" style=\"width:100%; padding:0px\"></div></div></div>");
+    mainDiv.append("<div class=\"mdl-cell mdl-cell--12-col\"><div class=\"demo-card-wide mdl-card mdl-shadow--2dp\" style=\"background: rgba(0,0,0,0.12)\"><div class=\"mdl-card__supporting-text\" id=\"product_grid\" style=\"width:100%; padding:0px\"></div></div></div>");
 
     mainDiv = $("#product_grid");
     mainDiv.append("<h1 style=\"margin: 24px 0px 0px 0px; padding: 0px 0px 0px 20px;\">Products</h1>");
@@ -63,7 +63,7 @@ function mainDivison()
     develop_Product();
 }
 function createCard(object,id){
-    object.append("<div class=\"mdl-cell mdl-cell--12-col\"><div class=\"demo-card-wide mdl-card mdl-shadow--2dp\" style=\"background: transparent\"><div class=\"mdl-card__supporting-text\" id=\"" +id + "\"></div></div></div>");
+    object.append("<div class=\"mdl-cell mdl-cell--12-col\"><div class=\"demo-card-wide mdl-card mdl-shadow--2dp\" style=\"background: rgba(0,0,0,0.12)\"><div class=\"mdl-card__supporting-text\" id=\"" +id + "\"></div></div></div>");
 }
 
 function cardWidthAdjustment() {
